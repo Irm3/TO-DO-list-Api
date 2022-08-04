2f45664f147343bff0ca4a6e36bac3988ad28fe4
Irm3 <irmantas.nainys@gmail.com>
2022-08-04 15:10:34 +0300

    add user and todolist models

---
+++ TO-DO list Api/Models/ToDoList.cs
@@ -0,0 +1,10 @@
+﻿namespace TO_DO_list_Api.Models
+{
+    public class ToDoList
+    {
+        private int TodoList_id { get; set; }
+        private string Name { get; set; } = null!;
+        private bool Status { get; set; }
+        public virtual User user { get; set; } = null!;
+    }
+}
---
+++ TO-DO list Api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TO_DO_list_Api.Models
+{
+    public class User
+    {
+        private int User_id { get; set; }
+        private string Email { get; set; } = null!;
+        private string Password { get; set; } = null!;
+        private string? Role { get; set; }
+
+
+    }
+}
