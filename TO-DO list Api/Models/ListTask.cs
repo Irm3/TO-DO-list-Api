eb6eefd42b1d854f8b7a58249326581b8c785b4e
Irm3 <irmantas.nainys@gmail.com>
2022-08-04 20:43:30 +0300

    add model annotations for email and password to be minimum of 12 characters.

--- TO-DO list Api/Models/ListTask.cs
+++ TO-DO list Api/Models/ListTask.cs
@@ -1,8 +1,12 @@
-﻿namespace TO_DO_list_Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TO_DO_list_Api.Models
 {
     public class ListTask
     {
+        [Required]
         public string Name { get; set; } = null!;
+        [Required]
         public bool Status { get; set; }
     }
 }
--- TO-DO list Api/Models/Login.cs
+++ TO-DO list Api/Models/Login.cs
@@ -1,9 +1,18 @@
-﻿namespace TO_DO_list_Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TO_DO_list_Api.Models
 {
     public class Login
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; } = null!;
 
+        [Required]
+        [StringLength(50, MinimumLength = 12,
+        ErrorMessage = "Password must be 12 symbols mininium and 50 maximum.")]
+        [DataType(DataType.Text)]
         public string Password { get; set; } = null!;
     }
 }
