1b0ce125c6f80854c1a3dc2dec34183628af05f2
Irm3 <irmantas.nainys@gmail.com>
2022-08-04 16:49:45 +0300

    create test methods for authorization, create ToDoList controller

--- TO-DO list Api/Controllers/MainController.cs
+++ TO-DO list Api/Controllers/MainController.cs
@@ -4,6 +4,7 @@ using TO_DO_list_Api.Models;
 using TO_DO_list_Api.JWT;
 using TO_DO_list_Api.Database;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Primitives;
 
 namespace TO_DO_list_Api.Controllers
 {
@@ -56,6 +57,30 @@ namespace TO_DO_list_Api.Controllers
             _context.SaveChanges();
 
             return Created("Success!", user);
+        }
+
+        /// <summary>
+        /// For testing logged in users
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet("testlogin")]
+        public IActionResult TestLogin()
+        {
+            Request.Headers.TryGetValue("Authorization", out StringValues authHeader);
+            return Ok(new { token = authHeader });
+        }
+
+        /// <summary>
+        /// For testing role specific users (admin)
+        /// </summary>
+        /// <returns></returns>
+        [Authorize(Roles = "role1")]
+        [HttpGet("testadmin")]
+        public IActionResult TestAdmin()
+        {
+            Request.Headers.TryGetValue("Authorization", out StringValues authHeader);
+            return Ok(new { token = authHeader });
         }
     }
 }
---
+++ TO-DO list Api/Controllers/ToDoListsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TO_DO_list_Api.Database;
+using TO_DO_list_Api.Models;
+
+namespace TO_DO_list_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoListsController : ControllerBase
+    {
+        private readonly ToDoListDBContext _context;
+
+        public ToDoListsController(ToDoListDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDoLists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoList>>> GetToDoLists()
+        {
+          if (_context.ToDoLists == null)
+          {
+              return NotFound();
+          }
+            return await _context.ToDoLists.ToListAsync();
+        }
+
+        // GET: api/ToDoLists/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoList>> GetToDoList(int id)
+        {
+          if (_context.ToDoLists == null)
+          {
+              return NotFound();
+          }
+            var toDoList = await _context.ToDoLists.FindAsync(id);
+
+            if (toDoList == null)
+            {
+                return NotFound();
+            }
+
+            return toDoList;
+        }
+
+        // PUT: api/ToDoLists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoList(int id, ToDoList toDoList)
+        {
+            if (id != toDoList.TodoListId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDoList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDoLists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ToDoList>> PostToDoList(ToDoList toDoList)
+        {
+          if (_context.ToDoLists == null)
+          {
+              return Problem("Entity set 'ToDoListDBContext.ToDoLists'  is null.");
+          }
+            _context.ToDoLists.Add(toDoList);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDoList", new { id = toDoList.TodoListId }, toDoList);
+        }
+
+        // DELETE: api/ToDoLists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoList(int id)
+        {
+            if (_context.ToDoLists == null)
+            {
+                return NotFound();
+            }
+            var toDoList = await _context.ToDoLists.FindAsync(id);
+            if (toDoList == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoLists.Remove(toDoList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoListExists(int id)
+        {
+            return (_context.ToDoLists?.Any(e => e.TodoListId == id)).GetValueOrDefault();
+        }
+    }
+}
